7c58af71394f7dab823de0abce1845252f7b531b
Emile DJIDA <emiledjida404@gmail.com>
2022-02-13 10:00:13 +0100

    <feature>:Data persistence added

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-import pymongo
+from pymongo import MongoClient
 from titanscraper import TitanScraper
 SOURCES_FILE = "sources.txt"
 
+
+mongo_client = MongoClient()
+db = mongo_client.jobboard
+jobs_collection = db.jobs_collection
 
 def load_sources():
     """Loads the file that contains all the source targets for the spider to scrap"""
@@ -23,6 +27,7 @@ def get_items_from_data(data):
 def main():
     """The main fuction, nothing to say about this"""
     scraper = TitanScraper()
+    logger = scraper.logger
     sources = load_sources()
 
     for source in sources:
@@ -31,7 +36,16 @@ def main():
 
         items = get_items_from_data(data)
         for item in items:
-            print(item['title'])
+            logger.info(f"Adding {item['link']} {[item['title']]} to collection")
+            # check if the item is already in the collection
+            if jobs_collection.find_one({'link' : item['link']}):
+                logger.info("Item already in collection")
+            else:
+                try:
+                    _id = jobs_collection.insert_one(item).inserted_id
+                    logger.info(f"Item added with id {_id}")
+                except Exception as e:
+                    logger.error(e)
 
 
 if __name__ == "__main__":
