d012d0051ed37b28308ef79548029b4ac4681493
Emile DJIDA <emiledjida404@gmail.com>
2022-02-13 14:22:02 +0100

    <feature>:Setup CORS policy

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@ import json
 
 from bson.json_util import dumps
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 
 mongo_client = MongoClient()
@@ -10,7 +11,19 @@ db = mongo_client.jobboard
 app = FastAPI()
 jobs_collection = db.jobs_collection
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/jobs")
 def jobs():
-    data = [ json.loads(dumps(job)) for job in jobs_collection.find({}, {"_id":0, "title":1, "pubDate":1, "link":1})]
+    data = [ json.loads(dumps(job)) for job in jobs_collection.find({}, {"_id":0, "description":0})]
     return data
